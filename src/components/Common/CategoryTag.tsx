7ee33cf8d910e196863c0d4e6e54b7f69f979641
Bukvity Ármin <bukvicarmin@gmail.com>
2026-02-13 13:56:41 +0100

    categorytag text translate

--- src/components/Common/CategoryTag.tsx
+++ src/components/Common/CategoryTag.tsx
@@ -5,9 +5,9 @@ interface Props {
 
 export default function CategoryTag({icon, category}: Props) {
     return (
-        <div className="border-[1px] border-[#292929] rounded-[50px] py-[9px] px-[11px] w-fit flex items-center gap-[5px]">
+        <div className="border-[1px] border-[#292929] rounded-[50px] py-[9px] px-[11px] w-fit flex items-center justify-center gap-[5px]">
             <img alt="category icon" src={icon} className="brightness-200" />
-            <p className="text-brightgray font-ppsemibold text-sm leading-[14px]">{category}</p>
+            <p className="text-brightgray font-ppsemibold text-sm leading-[100%] translate-y-[1.5px]">{category}</p>
         </div>
     )
 } 
